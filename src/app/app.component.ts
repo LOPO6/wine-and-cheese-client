1fb2d46dc9363e41b9369733e2721391511cc905
Logan <lbpotopn@lakeheadu.ca>
2025-02-27 15:27:58 -0500

    Angular setup, added basic info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+
 })
 export class AppComponent {
-  title = 'WINE-AND-CHEESE-CLIENT';
+  title = 'The Wine and Cheese Site';
 }
