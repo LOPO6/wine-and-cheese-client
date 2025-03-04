066df5e3d09f209224635f9f06cd386f4d2612c3
Logan <lbpotopn@lakeheadu.ca>
2025-03-04 17:19:24 -0500

    Bugs here needed fixing, cors rejecting the request, but still recieving the url

--- src/app/components/cheese/cheese.component.ts
+++ src/app/components/cheese/cheese.component.ts
@@ -1,21 +1,65 @@
-import { Component } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { CheeseService } from '../../services/cheese.service';
+import { FormsModule } from '@angular/forms';
 
-export class Cheese{
-  _id: string | undefined;
-  name: string | undefined;
-}
+// export class Cheese {
+//   _id: string | undefined;
+//   name: string | undefined;
+// }
 
 @Component({
   selector: 'app-cheese',
-  imports: [NgFor],
+  imports: [NgFor, NgIf, FormsModule],
   templateUrl: './cheese.component.html'
 })
-export class CheeseComponent {
-  CHEESES: Cheese[] =[
-    {_id: 'abc123', name: 'Cheddar'},
-    {_id: 'def456', name: 'Gouda'},
-    {_id: 'ghi789', name: 'Feta'}
-  ];
+export class CheeseComponent implements OnInit {
 
+  CHEESES: any;
+  _id: string | undefined;
+  name: string | undefined;
+  price: number | undefined;
+  stinkRating: number | undefined;
+  category: string | undefined;
+
+  constructor(private service: CheeseService) {}
+
+  getCheeses(): void {
+    this.service.getCheeses().subscribe(response => {
+      this.CHEESES = response;
+    })
+  }
+
+  addCheese(): void {
+    // create new object from form properties
+    let newCheese = {
+      name: this.name,
+      price: this.price,
+      stinkRating: this.stinkRating,
+      category: this.category
+    };
+
+    // pass to service
+    this.service.addCheese(newCheese).subscribe(response => {
+      this.getCheeses();
+      this.resetForm();
+    });
+  }
+
+  resetForm(): void {
+    this.name = undefined;
+    this.price = undefined;
+    this.stinkRating = undefined;
+    this.category = undefined;
+  }
+
+  ngOnInit() {
+    this.getCheeses();
+  }
+
+  // CHEESES: Cheese[] = [
+  //   { _id: 'abc123', name: 'Cheddar' },
+  //   { _id: 'def456', name: 'Gouda' },
+  //   { _id: 'ghi789', name: 'Feta' }
+  // ];
 }
