f35c145204807cdd3a44f1e05ec0af208c944426
Logan <lbpotopn@lakeheadu.ca>
2025-03-24 16:58:34 -0400

    Week 11 changes (part 1)

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, NgClass],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username: string |undefined;
+  password: string|undefined;
+  confirm: string|undefined;
+  message: string = 'Please choose a strong password';
+  messageClass: string = 'alert alert-info'
+  apiResponse: any;
+
+  constructor(private authService: AuthService){}
+
+  register(){
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+    //confirm password match
+    if(this.password != this.confirm){
+      this.message = 'Passowrd does not match';
+      this.messageClass = 'alert alert-danger'
+      return;
+    }
+    else if (this.password != undefined && regex.test(this.password) === false){
+      this.message = 'Invalid Password';
+      this.messageClass = 'alert alert-danger'
+      return;
+    }
+    
+
+    // create new user json object
+    const user = {
+      username: this.username,
+      password: this.password
+    };
+
+    // call service, which calls API, wait for response
+    return this.authService.register(user).subscribe({
+      next: response=>{
+        this.apiResponse = response;
+        //console.log(this.apiResponse);
+        this.message = 'Registration successful';
+        this.messageClass = 'alert alert-success';
+      },
+      error: err => {
+        this.message = err.message;
+        this.messageClass = 'alert alert-danger'
+      }
+    })
+  }
 
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  //read domain of API form env file, make available in this file for all methods
+  serverUrl: string = environment.serverUrl;
+  
+
+
+  //username var shared across components
+  private usernameSource = new BehaviorSubject<string |   null>(null);
+  username = this.usernameSource.asObservable(); //other components can subscribe or watch this value
+
+  setUsername(username: string): void{
+    this.usernameSource.next(username);
+  }
+
+  //remove global username on logout
+  clearUsername(): void{
+    this.usernameSource.next(null);
+  }
+
+  constructor(private http: HttpClient) { }
+
+  register(user: any){
+    return this.http.post(`${this.serverUrl}/users/register`, user);
+  }
+
+  login(user: any){
+    //withCredentials: true param allows browser to receive token w/JWT back from API
+    return this.http.post(`${this.serverUrl}/users/login`, user, {withCredentials: true});
+  }
+
+  logout(){
+    // pass authToken to API so server can remove it
+    return this.http.get(`${this.serverUrl}/users/logout`, {withCredentials: true});
+  }
+}
+
